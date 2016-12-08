c4dcf05ace0e11edc6cf2efd21d045f1342bf6a3
Snake575 <sarang575@gmail.com>
2016-12-08 18:41:31 -0300

    Updated url on setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='surbtc',
       version='0.1.0',
       description='SURBTC API wrapper for Python 3',
-      url='https://github.com/sarang575/surbtc',
+      url='https://github.com/delta575/python-surbtc-api',
       author='Felipe Aránguiz, Sebastian Aránguiz',
       authoremail='faranguiz575@gmail.com, sarang575@gmail.com',
       license='MIT',
