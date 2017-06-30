565701d8d28e06e7f0e2b70130c240e8b984e7d0
Felipe Aranguiz <faranguiz575@gmail.com>
2017-06-30 10:51:34 -0400

    bumped version to 0.2.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if not (sys.version_info >= (3, 5)):
 
 setup(
     name='surbtc',
-    version='0.2.1',
+    version='0.2.3',
     description='SURBTC API Wrapper for Python 3',
     url='https://github.com/delta575/python-surbtc-api',
     author='Felipe Aránguiz, Sebastian Aránguiz',
